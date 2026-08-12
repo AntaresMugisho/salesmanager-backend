61d62fb7169594f0a15df483ec00617b513b3f74
AntaresMugisho <antaresmugisho@gmail.com>
2026-08-12 06:11:24 +0200

    Add django_extentions

--- stockmanager/settings.py
+++ stockmanager/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt.token_blacklist",
+    "django_extensions",
     "corsheaders",
     "django_filters",
     "drf_spectacular",
